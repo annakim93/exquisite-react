66321128c99e3b5baae8ddbac8b15de2b94c181f
Anna Kim <anna.kim93@gmail.com>
2021-01-05 18:58:32 -0500

    add state for playerNum and lines of poetry

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -3,24 +3,19 @@ import PropTypes from 'prop-types';
 
 import './PlayerSubmissionForm.css';
 
-const PlayerSubmissionForm = () => {
+const PlayerSubmissionForm = (props) => {
   return (
     <div className="PlayerSubmissionForm">
       <h3>Player Submission Form for Player #{  }</h3>
-
       <form className="PlayerSubmissionForm__form" >
-
         <div className="PlayerSubmissionForm__poem-inputs">
-
           {
             // Put your form inputs here... We've put in one below as an example
           }
           <input
             placeholder="hm..."
             type="text" />
-
         </div>
-
         <div className="PlayerSubmissionForm__submit">
           <input type="submit" value="Submit Line" className="PlayerSubmissionForm__submit-btn" />
         </div>
