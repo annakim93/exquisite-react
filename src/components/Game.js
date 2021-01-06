3e9d3b7ef38d629acd7f83a4dbcc7a2a40f83702
Anna Kim <anna.kim93@gmail.com>
2021-01-05 19:04:06 -0500

    pass fields constant to player submisison form component thru props

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,38 +4,6 @@ import PlayerSubmissionForm from './PlayerSubmissionForm';
 import FinalPoem from './FinalPoem';
 import RecentSubmission from './RecentSubmission';
 
-const Game = () => {
-  const exampleFormat = FIELDS.map((field) => {
-    if (field.key) {
-      return field.placeholder;
-    } else {
-      return field;
-    }
-  }).join(' ');
-
-  const [playerNum, setPlayerNum] = useState(1);
-  const [poemLines, setPoemLines] = useState([]);
-
-  const addLine = () => {
-
-  };
-
-  return (
-    <div className="Game">
-      <h2>Game</h2>
-      <p>Each player should take turns filling out and submitting the form below. Each turn should be done individually and <em>in secret!</em> Take inspiration from the revealed recent submission. When all players are finished, click the final button on the bottom to reveal the entire poem.</p>
-      <p>Please follow the following format for your poetry submission:</p>
-      <p className="Game__format-example">
-        { exampleFormat }
-      </p>
-      <RecentSubmission />
-      <PlayerSubmissionForm index={ playerNum } sendSubmission={ addLine } />
-      <FinalPoem />
-    </div>
-  );
-}
-
-
 const FIELDS = [
   'The',
   {
@@ -65,5 +33,38 @@ const FIELDS = [
   },
   '.',
 ];
+
+
+const Game = () => {
+  const exampleFormat = FIELDS.map((field) => {
+    if (field.key) {
+      return field.placeholder;
+    } else {
+      return field;
+    }
+  }).join(' ');
+
+  const [playerNum, setPlayerNum] = useState(1);
+  const [poemLines, setPoemLines] = useState([]);
+
+  const addLine = () => {
+
+  };
+
+  return (
+    <div className="Game">
+      <h2>Game</h2>
+      <p>Each player should take turns filling out and submitting the form below. Each turn should be done individually and <em>in secret!</em> Take inspiration from the revealed recent submission. When all players are finished, click the final button on the bottom to reveal the entire poem.</p>
+      <p>Please follow the following format for your poetry submission:</p>
+      <p className="Game__format-example">
+        { exampleFormat }
+      </p>
+      <RecentSubmission />
+      <PlayerSubmissionForm index={ playerNum } sendSubmission={ addLine } fields={FIELDS} />
+      <FinalPoem />
+    </div>
+  );
+}
+
 
 export default Game;
